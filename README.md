fd76ef860deca3158004f862fd44dd0f84146cad
DhyanVGowda <dhyansanvi@gmail.com>
2025-01-04 17:09:01 -0600

    Initial commit with my modifications

---
+++ README.md
@@ -0,0 +1,28 @@
+# <a href="https://dhyanvgowda.github.io/" target="_blank">My Portfolio</a>
+
+<p style="text-align: justify;">This website showcases my projects, online presence, story, work experience, and contact information.</p>
+
+[![Repository Status](https://img.shields.io/badge/Repository%20Status-Maintained-dark%20green.svg)](https://github.com/dhyanvgowda/.github.io)
+[![Website Status](https://img.shields.io/badge/Website%20Status-Online-green)](https://dhyanvgowda.github.io/)
+[![Author](https://img.shields.io/badge/Author-Dhyan%20Vasudeva%20Gowda-purple.svg)]
+[![Latest Release](https://img.shields.io/badge/Latest%20Release-03%20Jan%202025-yellow.svg)](https://github.com/dhyanvgowda/dhyanvgowda.github.io)
+<a href="https://github.com/dhyanvgowda/dhyanvgowda.github.io/blob/master/LICENSE"><img alt="License" src="http://img.shields.io/:license-mit-blue.svg?style=flat-square?style=flat-square" /></a>
+
+<img width="100%" alt="Portfolio Screenshot" src="https://github.com/dhyanvgowda/dhyanvgowda.github.io/assets/86096184/fd0d70b2-1505-4adf-8b0c-427df58607e3">
+
+:star: Star me on GitHub — it helps!
+
+# Sections 📚
+
+✔️ Mini intro\
+✔️ About me \
+✔️ Skills\
+✔️ Projects\
+✔️ Experience\
+✔️ Contact me
+
+## License 📄
+
+This project is licensed under the MIT License - see the [LICENSE.md](./LICENSE) file for details.
+
+Please contact me at dhyansanvi@gmail.com if you have any feedback for the website. :star: Star it, if you like it!
---
+++ main.js
@@ -0,0 +1,199 @@
+var audio = document.getElementById("audioPlayer"),
+  loader = document.getElementById("preloader");
+function settingtoggle() {
+  document
+    .getElementById("setting-container")
+    .classList.toggle("settingactivate"),
+    document
+      .getElementById("visualmodetogglebuttoncontainer")
+      .classList.toggle("visualmodeshow"),
+    document
+      .getElementById("soundtogglebuttoncontainer")
+      .classList.toggle("soundmodeshow");
+}
+function playpause() {
+  !1 == document.getElementById("switchforsound").checked
+    ? audio.pause()
+    : audio.play();
+}
+function visualmode() {
+  document.body.classList.toggle("light-mode"),
+    document.querySelectorAll(".needtobeinvert").forEach(function (e) {
+      e.classList.toggle("invertapplied");
+    });
+}
+window.addEventListener("load", function () {
+  (loader.style.display = "none"),
+    document.querySelector(".hey").classList.add("popup");
+});
+let emptyArea = document.getElementById("emptyarea"),
+  mobileTogglemenu = document.getElementById("mobiletogglemenu");
+function hamburgerMenu() {
+  document.body.classList.toggle("stopscrolling"),
+    document
+      .getElementById("mobiletogglemenu")
+      .classList.toggle("show-toggle-menu"),
+    document
+      .getElementById("burger-bar1")
+      .classList.toggle("hamburger-animation1"),
+    document
+      .getElementById("burger-bar2")
+      .classList.toggle("hamburger-animation2"),
+    document
+      .getElementById("burger-bar3")
+      .classList.toggle("hamburger-animation3");
+}
+function hidemenubyli() {
+  document.body.classList.toggle("stopscrolling"),
+    document
+      .getElementById("mobiletogglemenu")
+      .classList.remove("show-toggle-menu"),
+    document
+      .getElementById("burger-bar1")
+      .classList.remove("hamburger-animation1"),
+    document
+      .getElementById("burger-bar2")
+      .classList.remove("hamburger-animation2"),
+    document
+      .getElementById("burger-bar3")
+      .classList.remove("hamburger-animation3");
+}
+const sections = document.querySelectorAll("section"),
+  navLi = document.querySelectorAll(".navbar .navbar-tabs .navbar-tabs-ul li"),
+  mobilenavLi = document.querySelectorAll(
+    ".mobiletogglemenu .mobile-navbar-tabs-ul li"
+  );
+window.addEventListener("scroll", () => {
+  let e = "";
+  sections.forEach((t) => {
+    let o = t.offsetTop;
+    t.clientHeight, pageYOffset >= o - 200 && (e = t.getAttribute("id"));
+  }),
+    mobilenavLi.forEach((t) => {
+      t.classList.remove("activeThismobiletab"),
+        t.classList.contains(e) && t.classList.add("activeThismobiletab");
+    }),
+    navLi.forEach((t) => {
+      t.classList.remove("activeThistab"),
+        t.classList.contains(e) && t.classList.add("activeThistab");
+    });
+}),
+  console.log(
+    "%c Designed and Developed by Dhyan Vasudeva Gowda ",
+    "background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;"
+  );
+let mybutton = document.getElementById("backtotopbutton");
+function scrollFunction() {
+  document.body.scrollTop > 400 || document.documentElement.scrollTop > 400
+    ? (mybutton.style.display = "block")
+    : (mybutton.style.display = "none");
+}
+function scrolltoTopfunction() {
+  (document.body.scrollTop = 0), (document.documentElement.scrollTop = 0);
+}
+(window.onscroll = function () {
+  scrollFunction();
+}),
+  document.addEventListener(
+    "contextmenu",
+    function (e) {
+      "IMG" === e.target.nodeName && e.preventDefault();
+    },
+    !1
+  );
+let Pupils = document.getElementsByClassName("footer-pupil"),
+  pupilsArr = Array.from(Pupils),
+  pupilStartPoint = -10,
+  pupilRangeX = 20,
+  pupilRangeY = 15,
+  mouseXStartPoint = 0,
+  mouseXEndPoint = window.innerWidth,
+  currentXPosition = 0,
+  fracXValue = 0,
+  mouseYEndPoint = window.innerHeight,
+  currentYPosition = 0,
+  fracYValue = 0,
+  mouseXRange = mouseXEndPoint - mouseXStartPoint;
+const mouseMove = (e) => {
+    (fracXValue =
+      (currentXPosition = e.clientX - mouseXStartPoint) / mouseXRange),
+      (fracYValue = (currentYPosition = e.clientY) / mouseYEndPoint);
+    let t = pupilStartPoint + fracXValue * pupilRangeX,
+      o = pupilStartPoint + fracYValue * pupilRangeY;
+    pupilsArr.forEach((e) => {
+      e.style.transform = `translate(${t}px, ${o}px)`;
+    });
+  },
+  windowResize = (e) => {
+    (mouseXEndPoint = window.innerWidth),
+      (mouseYEndPoint = window.innerHeight),
+      (mouseXRange = mouseXEndPoint - mouseXStartPoint);
+  };
+window.addEventListener("mousemove", mouseMove),
+  window.addEventListener("resize", windowResize);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const greetings = ["Namaste", "Hello", "Namaskara", "Hola"];
+  let currentIndex = 0;
+
+  function cycleGreeting() {
+    const dynamicGreetingElement = document.getElementById("dynamic-greeting");
+    if (dynamicGreetingElement) {
+      dynamicGreetingElement.textContent = greetings[currentIndex];
+      currentIndex = (currentIndex + 1) % greetings.length;
+    }
+  }
+
+  function getTimeBasedGreeting() {
+    const hours = new Date().getHours();
+    if (hours < 12) {
+      return "Hey, Good Morning!";
+    } else if (hours < 18) {
+      return "Hey, Good Afternoon!";
+    } else {
+      return "Hey, Good Evening!";
+    }
+  }
+
+  function updateGreeting() {
+    const greetingElement = document.getElementById("greeting");
+    if (greetingElement) {
+      greetingElement.innerText = getTimeBasedGreeting();
+    }
+  }
+
+  function showGreeting() {
+    updateGreeting();
+    const greetingElement = document.getElementById("greeting");
+    if (greetingElement) {
+      greetingElement.classList.add("show");
+      console.log("Greeting shown!");
+    }
+  }
+
+  function hideGreeting() {
+    const greetingElement = document.getElementById("greeting");
+    if (greetingElement) {
+      greetingElement.classList.remove("show");
+      console.log("Greeting hidden!");
+    }
+  }
+
+  const emoji = document.getElementById("nav-avatar");
+  if (emoji) {
+    emoji.addEventListener("mouseover", showGreeting);
+    emoji.addEventListener("mouseout", hideGreeting);
+  }
+
+  updateGreeting();
+  cycleGreeting();
+
+  setInterval(cycleGreeting, 2000);
+
+  window.addEventListener("load", () => {
+    if (window.location.hash) {
+      history.replaceState(null, null, " ");
+    }
+    window.scrollTo(0, 0);
+  });
+});
